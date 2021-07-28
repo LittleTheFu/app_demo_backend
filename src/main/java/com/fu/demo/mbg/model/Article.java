7ad5f54f381092104be1ce15502403ef0bb09222
LittleTheFu <lttlstpd@live.com>
2021-07-28 17:23:18 +0800

    add serialVersionUID

--- src/main/java/com/fu/demo/mbg/model/Article.java
+++ src/main/java/com/fu/demo/mbg/model/Article.java
@@ -9,28 +9,30 @@ public class Article implements Serializable {
 	private String title;
 
 	private String content;
-	
-	 public Long getId() {
-	        return id;
-	    }
 
-	    public void setId(Long id) {
-	        this.id = id;
-	    }
-	    
-	    public String getTitle() {
-	        return title;
-	    }
+	private static final long serialVersionUID = 1L;
 
-	    public void setTitle(String title) {
-	        this.title = title;
-	    }
+	public Long getId() {
+		return id;
+	}
 
-	    public String getContent() {
-	        return content;
-	    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-	    public void setContent(String content) {
-	        this.content = content;
-	    }
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
 }
