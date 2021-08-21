6dadf503c39d6df0c7180f78411ba5294a6ea465
LittleTheFu <lttlstpd@live.com>
2021-08-21 11:11:46 +0800

    add minio

---
+++ src/main/java/com/fu/demo/mbg/dto/MinioUploadDto.java
@@ -0,0 +1,27 @@
+package com.fu.demo.mbg.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+
+public class MinioUploadDto {
+	@ApiModelProperty(value = "name")
+	private String name;
+	
+	@ApiModelProperty(value = "url")
+	private String url;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+}
