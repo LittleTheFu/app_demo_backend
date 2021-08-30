28ee762259c401f9355bcf19e888053b9a5e7c8d
LittleTheFu <lttlstpd@live.com>
2021-08-30 10:42:26 +0800

    update HistoryResponseDto fields

--- src/main/java/com/fu/demo/mbg/dto/HistoryResponseDto.java
+++ src/main/java/com/fu/demo/mbg/dto/HistoryResponseDto.java
@@ -8,6 +8,39 @@ public class HistoryResponseDto {
 	
 	@ApiModelProperty(value = "title")
 	private String title;
+	
+	@ApiModelProperty(value = "authorName")
+	private String authorName;
+	
+	@ApiModelProperty(value = "authorIcon")
+	private String authorIcon;
+	
+	@ApiModelProperty(value = "authorId")
+	private long authorId;
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+
+	public String getAuthorIcon() {
+		return authorIcon;
+	}
+
+	public void setAuthorIcon(String authorIcon) {
+		this.authorIcon = authorIcon;
+	}
+
+	public long getAuthorId() {
+		return authorId;
+	}
+
+	public void setAuthorId(long authorId) {
+		this.authorId = authorId;
+	}
 
 	public long getId() {
 		return id;
