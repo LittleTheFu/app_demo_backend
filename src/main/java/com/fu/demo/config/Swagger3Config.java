772fff5030bfb8ab32eb543b828445358f41a87a
LittleTheFu <lttlstpd@live.com>
2021-09-19 11:11:35 +0800

    rename swagger file

--- src/main/java/com/fu/demo/config/Swagger3Config.java
+++ src/main/java/com/fu/demo/config/Swagger3Config.java
@@ -24,8 +24,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * Swagger2API文档的配置
  */
 @Configuration
-@EnableSwagger2
-public class Swagger2Config {
+public class Swagger3Config {
 	@Bean
 	public Docket createRestApi() {
 		return new Docket(DocumentationType.OAS_30) // v2 不同
