08c9d8d44d783a92e06c80f0b2205db18ceaaec3
LittleTheFu <lttlstpd@live.com>
2021-09-19 11:01:52 +0800

    update to swagger3

--- src/main/java/com/fu/demo/DemoApplication.java
+++ src/main/java/com/fu/demo/DemoApplication.java
@@ -3,6 +3,9 @@ package com.fu.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.oas.annotations.EnableOpenApi;
+
+@EnableOpenApi
 @SpringBootApplication
 public class DemoApplication {
 
--- src/main/java/com/fu/demo/config/Swagger2Config.java
+++ src/main/java/com/fu/demo/config/Swagger2Config.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.jsonwebtoken.lang.Collections;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,6 +14,7 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -25,39 +28,49 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class Swagger2Config {
 	@Bean
 	public Docket createRestApi() {
-		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
-				// 为当前包下controller生成API文档
-				.apis(RequestHandlerSelectors.basePackage("com.fu.demo.controller"))
-				// 为有@Api注解的Controller生成API文档
-//                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
-				// 为有@ApiOperation注解的方法生成API文档
-//                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
-				.paths(PathSelectors.any()).build().securitySchemes(securitySchemes())
+		return new Docket(DocumentationType.OAS_30) // v2 不同
+				.select().apis(RequestHandlerSelectors.basePackage("com.fu.demo")) // 设置扫描路径
+				.build()
+				.securitySchemes(securitySchemes())
 				.securityContexts(securityContexts());
 	}
-
-	private List<ApiKey> securitySchemes() {
+//	@Bean
+//	public Docket createRestApi() {
+//		return new Docket(DocumentationType.OAS_30).apiInfo(apiInfo()).select()
+//				// 为当前包下controller生成API文档
+//				.apis(RequestHandlerSelectors.basePackage("com.fu.demo.controller"))
+//				// 为有@Api注解的Controller生成API文档
+////                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+//				// 为有@ApiOperation注解的方法生成API文档
+////                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+////				.paths(PathSelectors.any()).build().securitySchemes(securitySchemes())
+////				.securityContexts(securityContexts());
+//				.build();
+//	}
+//
+	private List<SecurityScheme> securitySchemes() {
 		// 设置请求头信息
-		List<ApiKey> result = new ArrayList<>();
+//		List<SecurityScheme> result = new ArrayList<SecurityScheme>();
 		ApiKey apiKey = new ApiKey("Authorization", "Authorization", "header");
-		result.add(apiKey);
-		return result;
+//		result.add(apiKey);
+//		return result;
+		return java.util.Collections.singletonList(apiKey);
 	}
-
+//
 	private List<SecurityContext> securityContexts() {
 		// 设置需要登录认证的路径
 		List<SecurityContext> result = new ArrayList<>();
 		result.add(getContextByPath("/greeting/.*"));
 		return result;
 	}
-	
+//	
 	 private SecurityContext getContextByPath(String pathRegex){
 	        return SecurityContext.builder()
 	                .securityReferences(defaultAuth())
-	                .forPaths(PathSelectors.regex(pathRegex))
+//	                .forPaths(PathSelectors.regex(pathRegex))
 	                .build();
 	    }
-
+//
 	    private List<SecurityReference> defaultAuth() {
 	        List<SecurityReference> result = new ArrayList<>();
 	        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
@@ -66,8 +79,8 @@ public class Swagger2Config {
 	        result.add(new SecurityReference("Authorization", authorizationScopes));
 	        return result;
 	    }
-
-	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder().title("SwaggerUI演示").description("demo").contact("macro").version("1.0").build();
-	}
+//
+////	private ApiInfo apiInfo() {
+////		return new ApiInfoBuilder().title("SwaggerUI演示").description("demo").contact("macro").version("1.0").build();
+////	}
 }
